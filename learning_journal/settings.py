99bcdce3eea76d22ac2afddef52f55fe4390b74a
FIXED-TERM Badlani Mohemmedsiddiq (BGSW/ETM1-HYD) <bal1hyd@bosch.com>
2026-01-14 12:05:51 +0530

    Fix: Switch to non-manifest WhiteNoise storage to fix build crash

--- learning_journal/settings.py
+++ learning_journal/settings.py
@@ -188,6 +188,6 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
