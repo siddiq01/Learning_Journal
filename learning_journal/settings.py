c54b7b0144fdea197f661a182b2d3c9234b67381
FIXED-TERM Badlani Mohemmedsiddiq (BGSW/ETM1-HYD) <bal1hyd@bosch.com>
2026-01-14 12:13:36 +0530

    Fix build: Use standard static storage to bypass manifest errors

--- learning_journal/settings.py
+++ learning_journal/settings.py
@@ -188,6 +188,6 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
